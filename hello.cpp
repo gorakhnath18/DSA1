adfe76f06d7a14c6c8530f8a2e21cb47ad7e9cad
gorakhnath18 <137605216+gorakhnath18@users.noreply.github.com>
2024-08-01 15:35:59 +0000

    variable

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,5 @@
  #include<iostream>
  using namespace std;
  int main(){
-     cout<<"hello duniya dff"<<endl;
+     cout<<"hello duniya fgjffhhf"<<endl;
  }
---
+++ variable.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int  a =123;
+   // cout<<a<<endl;
+    char b = 'g';
+    cout<<a<<endl;
+    cout<<b<<endl;
+
+
+  }
