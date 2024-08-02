e63566b79191cb2cb98b735ae27b3cf0f3278193
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-02 11:29:52 +0000

    ifconditions

---
+++ ifcondition.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a= 24;
+   // cin>>n;
+   // cout<<"hello";
+   //cout<<"value od n is:"<<n<<endl;
+   if(a>20){
+    cout<<"love"<<endl;
+   }
+   else if(a==24){
+        cout<<"lovely"<<endl;
+
+   }
+   else{
+    cout<<"babbar"<<endl;
+   }
+
+   }
+   
