4ffd9302912aca0c364cd666691cd45710dcee50
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-02 13:31:47 +0000

    pattern

---
+++ pattern12.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int i=1;
+     char start ='A';
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+
+            cout<<start;
+            start=start+1;
+
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
