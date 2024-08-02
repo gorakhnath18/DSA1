49920e1c2391b43172c8c75598014a8cb568b2c1
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-02 12:12:29 +0000

    sumofeven number

---
+++ sumof n even numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+     cin>>n;
+     int i=2;
+     int sum=0;
+      while(i<=n){
+        if(i%2==0){
+
+        
+        sum=sum+i;
+        }
+         i++;
+      }
+      cout<<sum<<endl;
+
+
+   }
+   
