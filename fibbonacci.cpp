4ac6e938614959210ec0adebdfda944a463018d0
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-04 12:10:20 +0000

    code

--- fibbonacci.cpp
+++ fibbonacci.cpp
@@ -1,28 +1,18 @@
 #include<iostream>
- #include<vector>
-using namespace std;
-int fib(int n, vector<int> &dp) {
-
-  // base case
-  if (n<= 1)
-
-    return n;
-
-  dp[n]= fib(n -1,dp)+ fib(n - 2,dp);
-}
-
-int main() {
-  int n;
-
-  cin >> n;
-
-  vector<int>dp(n + 1);
+  using namespace std;
+  	int fib(int n)
+	{
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		else
+			return fib(n-1) + fib(n -2);
+	}
+    int main(){
+        int n;
+        cin>>n;
+        cout<<fib(n-1);
 
 
-  for (int i = 0; i <= n; i++)
-  dp[i]=-1;
-
-cout << fib(n, dp) << endl;
-
-  return 0;
-}
+    }
