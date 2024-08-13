4d77b9da804fbaf1608146b46a876f8cbe13ae61
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-13 13:43:35 +0000

    code

---
+++ lecture20.cpp
@@ -0,0 +1,42 @@
+/*#include<iostream>
+#include<vector>
+using namespace std;
+ vector<int> reverse(vector<int>v){
+    //int m;
+    //cin>>m;   for changing  from mth  position
+    int s=0;
+    int e=v.size()-1;
+    while(s<=e){
+        swap(v[s],v[e]);
+        s++;
+        e--;
+
+    }
+    return v;
+
+
+ }
+ void print (vector<int>v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+ }
+
+ int main(){
+    vector<int>v;
+    v.push_back(10);
+    v.push_back(12);
+    v.push_back(110);
+    v.push_back(1);
+    v.push_back(120);
+    v.push_back(129);
+    v.push_back(19);
+    v.push_back(14);
+    vector<int> ans= reverse(v);
+    print(ans);
+
+ }*/
+
+
+
