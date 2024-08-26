6a40d9c05bdc0486d827ea3ddbdfa402a344f667
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-08-26 11:02:27 +0000

    code

--- leetc.cpp
+++ leetc.cpp
@@ -1,20 +1,14 @@
- vector<int> result;
-        if (!root) return result;  // Edge case: if root is null
-        
-        stack<Node*> stk;
-        stk.push(root);
-        
-        while (!stk.empty()) {
-            Node* node = stk.top();
-            stk.pop();
-            result.push_back(node->val);
-            for (Node* child : node->children) {
-                stk.push(child);
-            }
-        }
-        
-        // Reverse the result to get postorder
-        reverse(result.begin(), result.end());
-        
-        return result;
+ #include<iostream>
+using namespace std;
+ void printcounting(int n){
+    for(int i =1;i<=n;i++){
+        cout<<i<<" ";
     }
+ }
+ int main(){
+    int n;
+    cin>>n;
+    printcounting(n);
+    return 0;
+
+ }
