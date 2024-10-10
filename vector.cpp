26875c88ddf8333bac9c7d6cf3885516f72a6db3
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2024-10-10 13:45:20 +0000

    code

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+ using namespace std;
+  int   main(){
+    vector <int> vec(3,1);
+    cout<<vec[0]<<endl;
+        cout<<vec[1]<<endl;
+    cout<<vec[2]<<endl;
+
+    return 0;
+  }
