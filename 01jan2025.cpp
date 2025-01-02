1f05d653d9a40dcb4cc18fceacc2346a4a4e0fb6
Gorakhnath jaiswal <137605216+gorakhnath18@users.noreply.github.com>
2025-01-02 06:11:21 +0000

    code

---
+++ 01jan2025.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main(){
+   /*int a =12;
+    int square=a*a;
+    cout<<square<<endl;*/
+
+     
+     
+     /* int a;
+    int b;
+    cin>>a;
+        cin>>b;
+
+    if(a<b){
+        cout<<a<<endl;
+    }
+    else{
+        cout<<b<<endl;
+    }
+    return 0;
+*/
+
+
+//even/odd
+/*int a;
+cin>>a;
+if(a%2==0){
+    cout<<"Even";
+}else{
+    cout<<"Odd";
+}
+
+*/
+//sum of 1 to n
+/*int n;
+cin>>n;
+int sum=0;
+for(int i=1;i<=n;i++){
+    sum=sum+i;
+}
+cout<<sum<<endl;*/
+
+
+
+
+return 0;
+}
